95356ca2552000e129aa43a4336b0c4bde2a8787
Andrey <starkovvandrey@gmail.ru>
2021-04-19 23:20:01 +0700

    Finaly

--- Morse.cs
+++ Morse.cs
@@ -3,97 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace Morse
+namespace Mors
 {
  
     class Morse
     {
-        public static void Morz(Dictionary<string, string> RUSL,string lang)  //Перевод в азбуку Морза
-        {
-            string path_Output = @"Output.txt";        // Путь к файлу, в котором будет хранится результат   
-            string f = @"Input.txt"; //Путь к файлу из которого мы берём текст
-            string result = "";
-            string temp1 = "";
-            string temp2 = "";
-            bool a = false; //Костыль, нужный чтоб определить, найден ли ненужный символ по типу запятой или нет)
-            using (StreamReader s = new StreamReader(f, true))
-            {
-                while ((temp1 = s.ReadLine()) != null)
-                {
-                    Console.WriteLine(temp1);
-                    result = "";
-                    for (int i = 0; i < temp1.Length; i++)
-                    {
-                            char c = temp1[i];
-                        if (lang == "ENG")   //Тут перевод из нижнего регистра в верхней
-                        {
-                            if ((c >= 'a') && (c <= 'z')) { c -= ' '; }
-                        }
-                        else if (c == 'ё') { c = 'Е'; }
-                        else
-                        if ((c >= 'а') && (c <= 'я')) { c -= ' '; }
-                            if (c == ' ') { result += " "; continue; } //Добавляет пробел, если был пробел в тексте, таким образом в переводе в Морзу будет 2 пробела перед следующим символом
-                            // И мы сможем восстановить этот пробел из текста
-                            temp2 += c;
-                        try
-                        {
-                            result += RUSL[temp2];
-                        }
-                        catch {
-                            temp2 = "";
-                            a = true;
-                        }
-                        if (!a) 
-                            //Мне просто нужно, чтобы при ошибке не добавлялось пробела, ради этого оно и сделано
-                        {
-                            temp2 = "";
-                            result += " ";
-                        }
-                        a = false;
-                    }
-                    using (StreamWriter ff = new StreamWriter(path_Output, true)) //Вывод в файл
-                        ff.WriteLine(result);
-                }
-            }
-        }
+        public Dictionary<string, string> L = new Dictionary<string, string> { [""] = "" };
+        public Dictionary<string, string> Reverse = new Dictionary<string, string> { [""] = "" };
 
-        public static void MorseReverse(Dictionary<string, string> RUSL,string f,string path_Output) //Перевод из азбуки Морза
+   
+
+
+        public Dictionary<string,string> RUS(Dictionary<string,string> RUSL)
         {
-            string result = "";
-            string temp1 = "", temp2 = "";
-            using (StreamReader s = new StreamReader(f, true)) {
-                while ((temp1 = s.ReadLine()) != null){
-                    result = ""; //Переведённая в итоге строка
-                    temp1 += " "; //Данная нам строка в которую мы должны перевести из Азбуки Морза
-                    for (int i = 0; i < temp1.Length; i++)
-                    {
-                       try
-                       {
-                            //   for (int i = 0; i < temp1.Length; i++)
-                            //   {
-                            if (temp1[i] != ' ') //В Морзе между всеми символами пишется пробел
-                            {
-                                temp2 += temp1[i];
-                            }
-                            else
-                            {
-                                result += RUSL[temp2];
-                                temp2 = "";
-                                if ((temp1[i + 1] == ' ')) { result += " "; } //Если 2 пробела подряд, то он меняет это на пробел в переведённой строке
-                            }
-                          }
-                        catch { }
-                    }
-                    using (StreamWriter ff = new StreamWriter(path_Output, true)) //Вывод в файл
-                        ff.WriteLine(result);
-                }
-                temp1 = "";
-            }
-        }
-           public static Dictionary<string,string> RUS(Dictionary<string,string> RUSL)
-        {
-            string path_lang = @"RUS.txt"; //Текстовый файл,в котором размещена библиотека языка
-            using (StreamReader f = new StreamReader(path_lang, true))  //Чтение экзампляра файла с помощью потоков
+            string path_lang = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\RUS.txt"; //Текстовый файл,в котором размещена библиотека языка
+            using (StreamReader f = new StreamReader(path_lang,true))  //Чтение экзампляра файла с помощью потоков
             {
                 string key = ""; //Значение для библиотеки
                 string value = ""; //Значение для библиотеки
@@ -119,7 +43,7 @@ namespace Morse
 
         public static Dictionary<string, string> RUSLReverse(Dictionary<string, string> RUSL) //Все оставшиеся языки по аналогии с предыдущем
         {
-            string path_lang = @"RUS.txt";
+            string path_lang = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\RUS.txt";
             using (StreamReader f = new StreamReader(path_lang, true))  
             {
                 string key = ""; 
@@ -149,7 +73,7 @@ namespace Morse
 
         public static Dictionary<string, string> ENG(Dictionary<string, string> RUSL)
         {
-            string path_lang = @"ENG.txt";
+            string path_lang = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\ENG.txt";
             using (StreamReader f = new StreamReader(path_lang, true))  
             {
                 string key = ""; 
@@ -178,7 +102,7 @@ namespace Morse
 
         public static Dictionary<string, string> ENGLReverse(Dictionary<string, string> RUSL) 
         {
-            string path_lang = @"ENG.txt";
+            string path_lang = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\ENG.txt";
             using (StreamReader f = new StreamReader(path_lang, true))  
             {
                 string key = ""; 
@@ -206,30 +130,107 @@ namespace Morse
         }
 
 
-        static public void Morsee() //Сюда загружаются все библиотеки языков, и выбирается язык
+        public Morse(string lang)
         {
-           
-            Dictionary<string, string> RUSL = new Dictionary<string, string> { [""] = "" };
-            Dictionary<string, string> RUSReverse = new Dictionary<string, string> { [""] = "" };
-            Dictionary<string, string> ENGL = new Dictionary<string, string> { [""] = "" };
-            Dictionary<string, string> ENGREVERSE = new Dictionary<string, string> { [""] = "" };
-            RUSL = RUS(RUSL); //Формирование библиотеки
-            RUSReverse = RUSLReverse(RUSReverse);
-            ENGL = ENG(ENGL);
-            ENGREVERSE = ENGLReverse(ENGREVERSE);
-            string choose; //Русский или английский
-            Console.WriteLine("RUS or ENG?");
-            choose = Console.ReadLine();
-            if (choose == "ENG")
+            if (lang == "RUS")
             {
-                Morz(ENGL,choose); //Переводит сначала в Морзу в один файл, выводит из Морзы обратно в другой файл
-                MorseReverse(ENGREVERSE, @"Output.txt", @"Finale.txt");
+                L = RUS(L);
+                Reverse = RUSLReverse(Reverse);
             }
-            if (choose=="RUS")
-            {
-                Morz(RUSL,choose);
-                MorseReverse(RUSReverse, @"Output.txt", @"Finale.txt");
-            }
+            else { 
+            L = ENG(L);
+            Reverse = ENGLReverse(Reverse);}
         }
+
+
+       public void printSOVIETSLOVar() {////////////////////////////////////////////////////////////
+            foreach (KeyValuePair<string, string> kvp in Reverse)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+}
+        //public static Dictionary<string,string> Morsa(string lang,bool a) //Сюда загружаются все библиотеки языков, и выбирается язык
+        //{
+        public string Morz(string temp1)  //Перевод в азбуку Морза
+        {
+           // printSOVIETSLOVar();
+            string path_Output = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\Output.txt";        // Путь к файлу, в котором будет хранится результат   
+            string f = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\Input.txt"; //Путь к файлу из которого мы берём текст
+            string result = "";
+            // string temp1 = "";
+          //  temp1.ToUpper();
+            string temp2 = "";
+            bool a = false; //Нужно чтоб определить, найден ли ненужный символ по типу запятой или нет)
+            result = "";
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                temp1=temp1.ToUpper();
+               // Console.WriteLine(temp1);
+                char c = temp1[i];
+                //Toupper(c);
+                //Char.ToUpper(c);
+                //Console.WriteLine();
+                if (c == ' ') { result += " "; continue; } //Добавляет пробел, если был пробел в тексте, таким образом в переводе в Морзу будет 2 пробела перед следующим символом
+                                                           // И мы сможем восстановить этот пробел из текста
+                temp2 += c;
+                try
+                {
+                    result += L[temp2];
+                }
+                catch
+                {
+                    temp2 = "";
+                    a = true;
+                }
+                if (!a)
+                //Мне просто нужно, чтобы при ошибке не добавлялось пробела, ради этого оно и сделано
+                {
+                    temp2 = "";
+                    result += " ";
+                }
+                a = false;
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public string MorseReverse(string f, string path_Output, string temp1) //Перевод из азбуки Морза
+        {
+            string result = "";
+            string temp2 = "";
+         //   printSOVIETSLOVar();
+          //  Console.WriteLine("aa");
+            {
+                result = ""; //Переведённая в итоге строка
+                temp1 += " "; //Данная нам строка в которую мы должны перевести из Азбуки Морза
+                for (int i = 0; i < temp1.Length; i++)
+                {
+                    try
+                    {
+                        if (temp1[i] != ' ') //В Морзе между всеми символами пишется пробел
+                        {
+                            temp2 += temp1[i];
+                        }
+                        else
+                        {
+                            result += Reverse[temp2];
+                            temp2 = "";
+                            if ((temp1[i + 1] == ' ')) { result += " "; } //Если 2 пробела подряд, то он меняет это на пробел в переведённой строке
+                        }
+                    }
+                    catch { }
+
+                }
+                temp1 = "";
+            }
+            //Console.WriteLine(result);
+            return result;
+        }
+
+        //    Dictionary<string, string> ENGL = new Dictionary<string, string> { [""] = "" };
+        //    Dictionary<string, string> ENGREVERSE = new Dictionary<string, string> { [""] = "" };
+
+        //}
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 
 namespace MorseN
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            Morse.Morse.Morsee();
+            string lang; //Русский или английский
+            string f1 = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\Input.txt";
+            string f2 = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\Output.txt";
+            string f3 = @"F:\06.04\MorseN\bin\Debug\netcoreapp2.0\Finale.txt";
+            Console.WriteLine("RUS or ENG?");
+            lang = Console.ReadLine();
+            lang.ToUpper();
+            Mors.Morse morzeTranslate = new Mors.Morse(lang);
+        //    morzeTranslate.printSOVIETSLOVar();//////////////////////////////////////////
+           // if (lang == "ENG") { language = ENGL; language2 = ENGREVERSE; } else { language = RUSL; language2 = RUSReverse; }
+            string temp1 = "";
+            {
+                using (StreamReader s = new StreamReader(f1, true))
+                {
+                    using (StreamWriter ff = new StreamWriter(f2))
+                    {
+                        while ((temp1 = s.ReadLine()) != null)
+                        {
+                            ff.WriteLine(morzeTranslate.Morz(temp1));
+                        }
+                    }
                 }
+
+                temp1 = "";
+                using (StreamReader s = new StreamReader(f2, true))
+                {
+                    using (StreamWriter ff = new StreamWriter(f3))
+                    {
+                        while ((temp1 = s.ReadLine()) != null)
+                        {
+                            ff.WriteLine(morzeTranslate.MorseReverse( f2, f3, temp1));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
+
